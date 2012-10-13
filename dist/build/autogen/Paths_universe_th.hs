6a37c02bf235d648147ef78bd05d5fa3b3c45282
Jonathan G Fischoff <jfischoff@Jonathans-MacBook-Pro.local>
2012-10-13 14:12:12 -0700

    drastically rewrote for the better

--- dist/build/autogen/Paths_universe_th.hs
+++ dist/build/autogen/Paths_universe_th.hs
@@ -4,24 +4,29 @@ module Paths_universe_th (
     getDataFileName
   ) where
 
+import qualified Control.Exception as Exception
 import Data.Version (Version(..))
 import System.Environment (getEnv)
+import Prelude
+
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+catchIO = Exception.catch
+
 
 version :: Version
-version = Version {versionBranch = [0,0,0,6], versionTags = []}
-
+version = Version {versionBranch = [0,1,0,0], versionTags = []}
 bindir, libdir, datadir, libexecdir :: FilePath
 
-bindir     = "/Users/hi5networks/.cabal/bin"
-libdir     = "/Users/hi5networks/.cabal/lib/universe-th-0.0.0.6/ghc-7.2.2"
-datadir    = "/Users/hi5networks/.cabal/share/universe-th-0.0.0.6"
-libexecdir = "/Users/hi5networks/.cabal/libexec"
+bindir     = "/Users/jfischoff/Library/Haskell/lib//bin"
+libdir     = "/Users/jfischoff/Library/Haskell/lib//lib/universe-th-0.1.0.0/x86_64/ghc-7.6.1"
+datadir    = "/Users/jfischoff/Library/Haskell/lib//share/universe-th-0.1.0.0"
+libexecdir = "/Users/jfischoff/Library/Haskell/lib//libexec"
 
 getBinDir, getLibDir, getDataDir, getLibexecDir :: IO FilePath
-getBinDir = catch (getEnv "universe_th_bindir") (\_ -> return bindir)
-getLibDir = catch (getEnv "universe_th_libdir") (\_ -> return libdir)
-getDataDir = catch (getEnv "universe_th_datadir") (\_ -> return datadir)
-getLibexecDir = catch (getEnv "universe_th_libexecdir") (\_ -> return libexecdir)
+getBinDir = catchIO (getEnv "universe_th_bindir") (\_ -> return bindir)
+getLibDir = catchIO (getEnv "universe_th_libdir") (\_ -> return libdir)
+getDataDir = catchIO (getEnv "universe_th_datadir") (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "universe_th_libexecdir") (\_ -> return libexecdir)
 
 getDataFileName :: FilePath -> IO FilePath
 getDataFileName name = do
